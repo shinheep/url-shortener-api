79d7e63560929d42d1a70e4e926a58e27f04ce7a
Shinhee Park <shinhee.park777@gmail.com>
2022-01-09 22:40:26 -0800

    Mobile nav complete

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,11 @@
+import "./Header.css"
+
 const Header = () => {
     return (
         <div className="header">
             <header>
                 <img src="images/logo.svg" alt=""/>
+                <i className="hamburger fas fa-bars"></i>
             </header>
         </div>
     );
