4445fa8a2eb6fd7174db7b48f5f82d82cd4f5c9d
Shinhee Park <shinhee.park777@gmail.com>
2022-01-30 14:51:15 -0800

    Menu toggle overlay, and fixed padding in Statistics component

--- src/Components/Header/Menu.js
+++ src/Components/Header/Menu.js
@@ -3,17 +3,26 @@ import "./Menu.css"
 const Menu = () => {
     return(
         <div className="menu">
-            <ul>
-                <li>
-                    <a href="/">Features</a>
-                </li>
-                <li>
-                    <a href="/">Pricing</a>
-                </li>
-                <li>
-                    <a href="/">Resources</a>
-                </li>
-            </ul>
+            <div className="first">
+                <ul>
+                    <li>
+                        <a href="/">Features</a>
+                    </li>
+                    <li>
+                        <a href="/">Pricing</a>
+                    </li>
+                    <li>
+                        <a href="/">Resources</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="second">
+                <ul>
+                    <li>Login</li>
+                    <li>Sign Up</li>
+                </ul>
+            </div>
         </div>
     );
 };
