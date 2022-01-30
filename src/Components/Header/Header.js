ca5051ca9ffdd2a90eb245cc1e811ab7d1eb40a5
Shinhee Park <shinhee.park777@gmail.com>
2022-01-30 13:55:36 -0800

    Hamburger menu toggle complete. Need styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,26 @@
 import "./Header.css";
+import {useEffect, useState} from 'react';
+import Menu from "./Menu";
+
 
 const Header = () => {
+    
+    const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        if (!isOpen) return;
+        const handleClose = () => setIsOpen(false);
+        window.addEventListener('click', handleClose);
+        return () => window.removeEventListener('click', handleClose);
+    }, [isOpen]);
+    
     return (
         <div className="header">
+            
+            {isOpen && <Menu/>}
             <header>
                 <img src="images/logo.svg" alt=""/>
-                <i className="hamburger fas fa-bars"></i>
+                <i onClick={() => setIsOpen(!isOpen)} className="hamburger fas fa-bars"></i>
             </header>
         </div>
     );
---
+++ src/Components/Header/Menu.js
@@ -0,0 +1,19 @@
+const Menu = () => {
+    return(
+        <div className="menu">
+            <ul>
+                <li>
+                    <a href="/">Features</a>
+                </li>
+                <li>
+                    <a href="/">Pricing</a>
+                </li>
+                <li>
+                    <a href="/">Resources</a>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Menu;
