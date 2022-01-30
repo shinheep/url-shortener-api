1423dcaee597e97e18636660e9cfc4a2f2da3b09
Shinhee Park <shinhee.park777@gmail.com>
2022-01-29 19:34:33 -0800

    Changed background color and added social icons to footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,28 +1,37 @@
+import "./Footer.css"
+
 const Footer = () => {
     return(
         <div className="footer">
             <h1>Shortly</h1>
 
-            <div className="features">
+            <div className="features section">
                 <h3>Features</h3>
                 <p>Link Shortening</p>
                 <p>Branded Links</p>
                 <p>Analytics</p>
             </div>
 
-            <div className="resources">
+            <div className="resources section">
                 <h3>Resources</h3>
                 <p>Blog</p>
                 <p>Developers</p>
                 <p>Support</p>
             </div>
 
-            <div className="company">
+            <div className="company section">
                 <h3>Company</h3>
                 <p>About</p>
                 <p>Our Team</p>
                 <p>Careers</p>
                 <p>Contact</p>
+            </div>
+
+            <div className="icons">
+                <img className="socialIcons" src="images/icon-facebook.svg" alt='facebook'/>
+                <img className="socialIcons" src="images/icon-twitter.svg" alt='twitter'/>
+                <img className="socialIcons" src="images/icon-pinterest.svg" alt='pinterest'/>
+                <img className="socialIcons" src="images/icon-instagram.svg" alt='instagram'/>
             </div>
         </div>
     );
